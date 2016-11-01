98900e978ade3de3666b58cd023c833b819f3725
andrewfasano <hello@andrewfasano.com>
2016-10-31 22:28:50 -0400

    Case insensitive check for base64 string

--- setup/files/html/inc.php
+++ setup/files/html/inc.php
@@ -1,5 +1,5 @@
 <?php
   if (!isset($_GET["p"])) die("error");
-  if (strpos($_GET["p"], "base64") !== false) die("Hacking attempt detected!");
+  if (stripos($_GET["p"], "base64") !== false) die("Hacking attempt detected!");
   include($_GET["p"] . ".php");
 ?>
