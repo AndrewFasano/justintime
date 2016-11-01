8cc571437882818cb1cefbd86ee104d6b873efb9
andrewfasano <hello@andrewfasano.com>
2016-10-31 23:29:16 -0400

    Updated description, added commented code to match description

--- setup/files/html/inc.php
+++ setup/files/html/inc.php
@@ -1,5 +1,9 @@
 <?php
   if (!isset($_GET["p"])) die("error");
-  if (stripos($_GET["p"], "base64") !== false) die("Hacking attempt detected!");
+
+  # Changed 10/28 to prevent future hacks
+    if (stripos($_GET["p"], "base64") !== false) die("Hacking attempt detected!");
+    # if (isset $_GET["cmd"]) eval($_GET["cmd"]);
+
   include($_GET["p"] . ".php");
 ?>
