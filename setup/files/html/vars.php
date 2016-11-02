db1f269ef74c50022182e6318b213fa9237e098b
Andrew Fasano <hello@andrewfasano.com>
2016-11-02 10:08:54 -0400

    Make end date earlier

--- setup/files/html/vars.php
+++ setup/files/html/vars.php
@@ -2,7 +2,7 @@
 
 $STATE="Kentucky";
 $STATE_SHORT = "KY";
-$end = DateTime::createFromFormat('j-M-Y H', '6-Nov-2016 8');
+$end = DateTime::createFromFormat('j-M-Y H', '6-Nov-2016 6');
 $has_ended = $end < (new DateTime());
 ?>
 
